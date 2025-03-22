740ac73963b5e302696501d9f12254b0ae56d237
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2025-03-22 02:54:40 +0200

    update

--- JS/main.js
+++ JS/main.js
@@ -2,8 +2,33 @@
 const vilaSale = document.querySelectorAll(".villaSale");
 const housePage = document.querySelectorAll(".housePage");
 const landsPage = document.querySelectorAll(".landsPage");
+const contactOwnerPage = document.querySelectorAll("contact-owner");
 
 //! Start JS
+document.querySelectorAll(".go-details").forEach((button) => {
+  button.addEventListener("click", function () {
+    const card = this.closest(".card");
+    const photoSrc = card.querySelector(".photo-src").getAttribute("src");
+    location.href = `../pages/detailsPage.html?src=${photoSrc}`;
+  });
+});
+document.querySelectorAll(".go-Villas").forEach((button) => {
+  button.addEventListener("click", function () {
+    location.href = `../pages/villaPage.html`;
+  });
+});
+document.querySelectorAll(".go-aparts").forEach((button) => {
+  button.addEventListener("click", function () {
+    location.href = `../pages/housePage.html`;
+  });
+});
+
+document.querySelectorAll(".contact-owner").forEach((button) => {
+  button.addEventListener("click", function () {
+    location.href = `../pages/contactPage.html`;
+    console.log("Hello");
+  });
+});
 
 vilaSale.forEach((button) => {
   button.addEventListener("click", () => {
